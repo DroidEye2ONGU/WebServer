c26a9ed45b361faceecc71d84d4c5874cfeb15ee
DroidEye <droideye.ongu@foxmail.com>
2018-07-08 14:45:44 +0800

    update

--- src/DroidEye/Util/ServerUtil.java
+++ src/DroidEye/Util/ServerUtil.java
@@ -9,7 +9,6 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
